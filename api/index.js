02f116470d1fe63c1d22998e37f64ed00f23a416
eslam2semedo <eslamsemedo@gmail.com>
2025-01-30 21:13:09 +0200

    CORS

--- api/index.js
+++ api/index.js
@@ -3,7 +3,13 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(cors({
+    origin: 'http://localhost:5173',  // Your React dev origin
+    methods: ['GET', 'POST', 'OPTIONS'],
+}));
+
+app.get("/retal", (req, res) => {
+    req.body
     res.status(200).json("eslam")
 })
 
